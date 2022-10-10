1c06fa08ccc734f01b0a5fbd2aea6fcdcd127092
Tanay Ojha <tanay.ojha@Yash.com>
2022-10-10 22:50:34 +0530

    orm task done

--- SpringOrmTask/orm/src/main/java/com/yash/orm/App.java
+++ SpringOrmTask/orm/src/main/java/com/yash/orm/App.java
@@ -19,10 +19,25 @@ public class App
         System.out.println( "Hello World!" );
         ApplicationContext context = new ClassPathXmlApplicationContext("com/yash/orm/config.xml");
         EmployeeDao empDao  =context.getBean("employeeDao",EmployeeDao.class);
-        Employee emp = new Employee(1,"Tanay","tanay.ojha@yash.com");
+        Employee emp = new Employee();
+        emp.setEmpId(1);
+        //emp.setEmpName("Tanay");
+        //emp.setEmail("tanay.ojha@yash.com");
+        //emp = new Employee("3","deepesh","deepesh@yash.com");
         //Insert data of employee into database;
-        empDao.insert(emp);
+        //empDao.insert(emp);
+        //emp.setEmpId(3);
+        //emp.setEmpName("deepesh");
+        //emp.setEmail("deepesh@yash.com");
+        //empDao.insert(emp);
         //Update data of employee into database
-        empDao.update(emp);
+        //empDao.update(emp);
+        //Delete data from database
+        //empDao.delete(emp);
+        //Get all employees detail from database
+        //empDao.getAllEmployeeList();
+        //Get employee detail from empId by database
+        empDao.getEmployeeById(emp.getEmpId());
+        
     }
 }
---
+++ junit/src/main/java/com/yash/junit/Program1.java
@@ -0,0 +1,13 @@
+package com.yash.junit;
+
+public class Program1 {
+
+	public int findConsecutive(int array[]) {
+	    int length = array.length;
+	    int firstNum = array[0];
+	    int lastNum = array[length - 1];
+	    int totalSum = length * (firstNum + lastNum) / 2;
+	    return totalSum;
+	}
+
+}
